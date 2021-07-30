0707ce1d64cb347fcb3fb0ef4716954d6bcb0f71
Huemanman <87739582+Huemanman@users.noreply.github.com>
2021-07-30 10:27:12 +1000

    Update Read_time.ino

--- Read_time/Read_time.ino
+++ Read_time/Read_time.ino
@@ -52,6 +52,10 @@ void setup() {
   //EINK
   display.begin(THINKINK_MONO);
   display.clearBuffer();
+
+  AFMS.begin();
+  myMotor->setSpeed(255);
+  logEvent("System Initialisation...");
 }
 
 void loop() {
@@ -123,4 +127,52 @@ void setupSD() {
     return;
   }
   Serial.println("SD Started");
+}
+
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file on an SD card.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+
+  // Open the log file
+  File logFile = SD.open("/logEvents.csv", FILE_APPEND);
+  if (!logFile) {
+    Serial.print("Couldn't create log file");
+    abort();
+  }
+
+  // Log the event with the date, time and data
+  logFile.print(rightNow.year(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.month(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.day(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.hour(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.minute(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.second(), DEC);
+  logFile.print(",");
+  logFile.print(dataToLog);
+
+  // End the line with a return character.
+  logFile.println();
+  logFile.close();
+  Serial.print("Event Logged: ");
+  Serial.print(rightNow.year(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.month(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.day(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.hour(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.minute(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.second(), DEC);
+  Serial.print(",");
+  Serial.println(dataToLog);
 }
