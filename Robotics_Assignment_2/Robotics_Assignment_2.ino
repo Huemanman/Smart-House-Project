be35ddd8b0560ec2fc58c97ad13f8c0924b710d8
Huemanman <0476653@schoolsnet.act.edu.au>
2021-09-03 14:03:23 +1000

    Update Robotics_Assignment_2.ino

--- Robotics_Assignment_2/Robotics_Assignment_2.ino
+++ Robotics_Assignment_2/Robotics_Assignment_2.ino
@@ -11,7 +11,7 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 #include <SPI.h>
 #include <SD.h>
 #include "FS.h"
- 
+
 // Set Variables and Find and Inclued Files for EINK
 #include "Adafruit_ThinkInk.h"
 #define EPD_CS      15
@@ -49,6 +49,22 @@ void setup() {
   // Setup SD Card
   setupSD();
 
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  loadHTML();
+
   // Setup RTC
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
@@ -72,6 +88,31 @@ void setup() {
 
   // Setup Soil Monitor
   readSoil();
+
+
+  // Webserver
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+
+  server.on("/", HTTP_GET, []() {
+    Serial.println("Index");
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    Serial.println("serverIndex");
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  server.begin();
+}
 }
 
 void loop() {
@@ -96,118 +137,143 @@ void loop() {
   // waits 180 seconds (3 minutes) as per guidelines from adafruit.
   delay(180000);
   display.clearBuffer();
-}
-//State How to Draw the Text on the E-ink Monitor
-void drawText(String text, uint16_t color, int textSize, int x, int y) {
-  display.setCursor(x, y);
-  display.setTextColor(color);
-  display.setTextSize(textSize);
-  display.setTextWrap(true);
-  display.print(text);
 
-}
-//State the Time as a String so the E-Ink Monitor can Display it
-String getDateTimeAsString() {
-  DateTime now = rtc.now();
+  //State How to Draw the Text on the E-ink Monitor
+  void drawText(String text, uint16_t color, int textSize, int x, int y) {
+    display.setCursor(x, y);
+    display.setTextColor(color);
+    display.setTextSize(textSize);
+    display.setTextWrap(true);
+    display.print(text);
 
-  //Prints the date and time to the Serial monitor for debugging.
-  /*
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-  */
-
-  // Converts the date and time into a human-readable format.
-  char humanReadableDate[20];
-  sprintf(humanReadableDate, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
-
-  return humanReadableDate;
-}
-
-//State What the Code Means When it Says "setupSD" in the Above Code
-void setupSD() {
-  if (!SD.begin()) {
-    Serial.println("Card Mount Failed");
-    return;
   }
-  uint8_t cardType = SD.cardType();
+  //State the Time as a String so the E-Ink Monitor can Display it
+  String getDateTimeAsString() {
+    DateTime now = rtc.now();
 
-  if (cardType == CARD_NONE) {
-    Serial.println("No SD card attached");
-    return;
-  }
-  Serial.println("SD Started");
-}
+    //Prints the date and time to the Serial monitor for debugging.
+    /*
+      Serial.print(now.year(), DEC);
+      Serial.print('/');
+      Serial.print(now.month(), DEC);
+      Serial.print('/');
+      Serial.print(now.day(), DEC);
+      Serial.print(" (");
+      Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+      Serial.print(") ");
+      Serial.print(now.hour(), DEC);
+      Serial.print(':');
+      Serial.print(now.minute(), DEC);
+      Serial.print(':');
+      Serial.print(now.second(), DEC);
+      Serial.println();
+    */
 
-//Find the Time and Date in the AdaLogger and Display to the Arduino
-void logEvent(String dataToLog) {
-  /*
-     Log entries to a file on an SD card.
-  */
-  // Get the updated/current time
-  DateTime rightNow = rtc.now();
+    // Converts the date and time into a human-readable format.
+    char humanReadableDate[20];
+    sprintf(humanReadableDate, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
 
-  // Open the log file
-  File logFile = SD.open("/logEvents.csv", FILE_APPEND);
-  if (!logFile) {
-    Serial.print("Couldn't create log file");
-    abort();
+    return humanReadableDate;
   }
 
-  // Log the event with the date, time and data
-  logFile.print(rightNow.year(), DEC);
-  logFile.print(",");
-  logFile.print(rightNow.month(), DEC);
-  logFile.print(",");
-  logFile.print(rightNow.day(), DEC);
-  logFile.print(",");
-  logFile.print(rightNow.hour(), DEC);
-  logFile.print(",");
-  logFile.print(rightNow.minute(), DEC);
-  logFile.print(",");
-  logFile.print(rightNow.second(), DEC);
-  logFile.print(",");
-  logFile.print(dataToLog);
+  //State What the Code Means When it Says "setupSD" in the Above Code
+  void setupSD() {
+    if (!SD.begin()) {
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    uint8_t cardType = SD.cardType();
 
-  // End the line with a return character.
-  logFile.println();
-  logFile.close();
-  Serial.print("Event Logged: ");
-  Serial.print(rightNow.year(), DEC);
-  Serial.print(",");
-  Serial.print(rightNow.month(), DEC);
-  Serial.print(",");
-  Serial.print(rightNow.day(), DEC);
-  Serial.print(",");
-  Serial.print(rightNow.hour(), DEC);
-  Serial.print(",");
-  Serial.print(rightNow.minute(), DEC);
-  Serial.print(",");
-  Serial.print(rightNow.second(), DEC);
-  Serial.print(",");
-  Serial.println(dataToLog);
-}
+    if (cardType == CARD_NONE) {
+      Serial.println("No SD card attached");
+      return;
+    }
+    Serial.println("SD Started");
+  }
 
-//Find the Moiture in the Soil Via the Soil Moisture Sensor and State to the Arduino
-int readSoil() {
-  moistValue = analogRead(soilPin);//Read the SIG value form sensor
-  return moistValue;//send current moisture value
-}
+  //Find the Time and Date in the AdaLogger and Display to the Arduino
+  void logEvent(String dataToLog) {
+    /*
+       Log entries to a file on an SD card.
+    */
+    // Get the updated/current time
+    DateTime rightNow = rtc.now();
 
-void waterPlant(int moistureValue) {
-  //The function is to be called waterPlant() which will take the
-  //moisture value as an argument, and return no value.
+    // Open the log file
+    File logFile = SD.open("/logEvents.csv", FILE_APPEND);
+    if (!logFile) {
+      Serial.print("Couldn't create log file");
+      abort();
+    }
 
-  
-}
+    // Log the event with the date, time and data
+    logFile.print(rightNow.year(), DEC);
+    logFile.print(",");
+    logFile.print(rightNow.month(), DEC);
+    logFile.print(",");
+    logFile.print(rightNow.day(), DEC);
+    logFile.print(",");
+    logFile.print(rightNow.hour(), DEC);
+    logFile.print(",");
+    logFile.print(rightNow.minute(), DEC);
+    logFile.print(",");
+    logFile.print(rightNow.second(), DEC);
+    logFile.print(",");
+    logFile.print(dataToLog);
+
+    // End the line with a return character.
+    logFile.println();
+    logFile.close();
+    Serial.print("Event Logged: ");
+    Serial.print(rightNow.year(), DEC);
+    Serial.print(",");
+    Serial.print(rightNow.month(), DEC);
+    Serial.print(",");
+    Serial.print(rightNow.day(), DEC);
+    Serial.print(",");
+    Serial.print(rightNow.hour(), DEC);
+    Serial.print(",");
+    Serial.print(rightNow.minute(), DEC);
+    Serial.print(",");
+    Serial.print(rightNow.second(), DEC);
+    Serial.print(",");
+    Serial.println(dataToLog);
+  }
+
+  //Find the Moiture in the Soil Via the Soil Moisture Sensor and State to the Arduino
+  int readSoil() {
+    moistValue = analogRead(soilPin);//Read the SIG value form sensor
+    return moistValue;//send current moisture value
+  }
+
+  void waterPlant(int moistureValue) {
+    //The function is to be called waterPlant() which will take the
+    //moisture value as an argument, and return no value.
+  }
+
+  String readFile(fs::FS & fs, const char * path) {
+    Serial.printf("Reading file: %s\n", path);
+    char c;
+    String tempHTML = "";
+
+    File file = fs.open(path);
+    if (!file) {
+      Serial.print("Failed to open file for reading: ");
+      Serial.println(path);
+      return "";
+    }
+
+    while (file.available()) {
+      c = file.read();
+      tempHTML += c;
+    }
+    file.close();
+    return tempHTML;
+  }
+
+
+
+  void loadHTML() {
+    serverIndex = readFile(SD, "/serverIndex.html");
+    loginIndex = readFile(SD, "/loginIndex.html");
+  }
